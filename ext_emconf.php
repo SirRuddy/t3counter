63566b859433726adbad6a917f44c16e0b236577
Philipp Mueller <philipp.mueller@lavitto.ch>
2020-04-06 12:01:27 +0200

    [TASK] Added support for TYPO3 10 LTS and PHP 7.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,8 +17,8 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'plugin',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
-            'php' => '7.1.0-7.3.99'
+            'typo3' => '8.7.0-10.4.99',
+            'php' => '7.1.0-7.4.99'
         ],
         'conflicts' => [],
         'suggests' => [
