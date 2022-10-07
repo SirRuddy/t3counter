e3ea8fe385be58029b45062821ce4c1b3cada426
Philipp Müller <philipp.mueller@lavitto.ch>
2022-10-07 16:46:57 +0200

    [TASK] Added compatibility for TYPO3 11

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,8 +17,8 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'plugin',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-10.4.99',
-            'php' => '7.1.0-7.4.99'
+            'typo3' => '8.7.0-11.5.99',
+            'php' => '7.1.0-8.1.99'
         ],
         'conflicts' => [],
         'suggests' => [
